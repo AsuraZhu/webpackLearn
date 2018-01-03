f82c34075fa9e42046746273344161ef8c9cdff4
zmm <1614180462@qq.com>
2018-01-03 17:14:50 +0800

    添加静态资源文件

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { B } from "./b";
-
-class A {
-    
-}
-new B();
-console.log(new A());
+Laya.init(600,300);
+const txt = new Laya.Text();
+txt.text = "Hello world";
+txt.color = "#ffffff";
+Laya.stage.addChild(txt);
+Laya.loader.load("res/atlas/gameUI.atlas",Laya.Handler.create(this,this.gameStart))
