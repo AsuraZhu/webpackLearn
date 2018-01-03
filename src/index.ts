f3b348defb25d24086e6da357804da7ce3c625ab
zmm <1614180462@qq.com>
2018-01-03 17:20:57 +0800

    添加 cdn文件

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-Laya.init(600,300);
-const txt = new Laya.Text();
-txt.text = "Hello world";
-txt.color = "#ffffff";
-Laya.stage.addChild(txt);
-Laya.loader.load("res/atlas/gameUI.atlas",Laya.Handler.create(this,this.gameStart))
+class Main {
+    gameStart(arg0: any, arg1: any): any {
+        throw new Error("Method not implemented.");
+    }
+    constructor() {
+        Laya.init(720,1280, Laya.WebGL);
+        Laya.Stat.show();
+        Laya.stage.scaleMode = Laya.Stage.SCALE_EXACTFIT;
+        Laya.loader.load("res/atlas/gameUI.atlas",Laya.Handler.create(this,this.gameStart))
+    }
+
+}
+
+new Main();
